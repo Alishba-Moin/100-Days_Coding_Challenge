b497bf8d2af557f8320ada0a2b0b7906142e0516
Alishba Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-05-14 19:09:15 +0500

    Upload day 46

---
+++ Day 46/qno136.js
@@ -0,0 +1,5 @@
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+for (var i = 1; i <= 5; i++) {
+    var variable = i * 2;
+    console.log("Value of i ".concat(i, ", Iteration: ").concat(variable));
+}
---
+++ Day 46/qno136.ts
@@ -0,0 +1,7 @@
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+
+
+for (let i = 1; i <= 5; i++) {
+    let variable = i * 2;
+    console.log(`Value of i ${i}, Iteration: ${variable}`)   
+  }
